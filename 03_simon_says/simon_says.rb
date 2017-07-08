f4d889e5f51efbc16b16cf520cb5f3fce71fa750
cesaracosta <cesar.acosta@gmail.com>
2017-07-07 21:53:30 -0700

    Ejercicio 3 Listo

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,14 +6,15 @@ def shout(text)
   text.upcase
 end
 
-def repeat(text)
-  text=text+" "+text
-end
-
-def repeat(text,num)
-  texts=text
-  while (num>1) do
-    texts=texts+" "+text
-    num=num-1
+def repeat(text,num=0)
+  if num == 0
+    text=text+" "+text
+    exit
+  else
+    texts=text
+    while (num>1) do
+      texts=texts+" "+text
+      num=num-1
+    end
   end
 end
