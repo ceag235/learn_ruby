08ebc029e6810d2d4bc17e346f2ad7b069716ade
cesaracosta <cesar.acosta@gmail.com>
2017-07-07 21:24:59 -0700

    Avances del Ejercio 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,19 @@
-#write your code here
+def echo(text)
+  text
+end
+
+def shout(text)
+  text.upcase
+end
+
+def repeat(text)
+  text=text+" "+text
+end
+
+def repeat(text,num)
+  texts=text
+  while (num>1) do
+    texts=texts+" "+text
+    num=num-1
+  end
+end
