dded794f9936265817711f6a4c244b004d15d3f2
Cesar Acosta <cesar.acosta@gmail.com>
2017-07-14 18:03:54 -0400

    Ejercicio 5 Avance
    Avance del ejercicio 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,36 @@
 class Book
-# write your code here
+
+  def title= book
+    @title=titleize(book)
+  end
+
+  def title
+    @title
+  end
+
+
+
+  def titleize (text)
+    caps = text.capitalize
+    caps = caps.split(" ")
+    word=""
+    cant =0
+    if caps.length > 1
+      word=caps[0]
+      while cant < caps.length-1 do
+        cant=cant+1
+        wordSub=caps[cant]
+        isVocal=wordSub.start_with?("a","e","i","o","u")
+        if wordSub.length >= 4 and isVocal==false
+          wordSub=wordSub.capitalize
+        end
+        word=word+" "+wordSub
+      end
+    else
+      word=caps[0]
+    end
+    word
+  end
+
+
 end
