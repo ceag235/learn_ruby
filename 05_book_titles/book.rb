679792a4bfe39f2cc010af34c03db9d7c9cdc299
cesaracosta <cesar.acosta@gmail.com>
2017-07-17 22:58:21 -0400

    Ejercicio 5 Completado

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -9,6 +9,20 @@ class Book
   end
 
 
+  def isArticle (text)
+    value = case text
+      when "a" then true
+      when "an" then true
+      when "and" then true
+      when "is" then true
+      when "are" then true
+      when "the" then true
+      when "of" then true
+      else false
+      end
+    value
+    end
+
 
   def titleize (text)
     caps = text.capitalize
@@ -22,6 +36,10 @@ class Book
         wordSub=caps[cant]
         isVocal=wordSub.start_with?("a","e","i","o","u")
         if wordSub.length >= 4 and isVocal==false
+          wordSub=wordSub.capitalize
+        elsif isArticle(wordSub)==false and wordSub.length >= 3
+          wordSub=wordSub.capitalize
+        elsif wordSub=="i"
           wordSub=wordSub.capitalize
         end
         word=word+" "+wordSub
